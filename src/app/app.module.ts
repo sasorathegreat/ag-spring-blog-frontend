a12611928f4d608e68149c77896962b99d9e2606
Crystal Logan <crystal_d_logan@outlook.com>
2020-07-03 18:54:25 -0400

    Created login and register-success component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterSucessComponent } from './auth/register-sucess/register-sucess.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    RegisterSucessComponent
   ],
   imports: [
     BrowserModule,
