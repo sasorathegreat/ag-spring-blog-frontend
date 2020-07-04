f22041ec4a0e3ac66662594df88abcd5cce7544b
Crystal Logan <crystal_d_logan@outlook.com>
2020-07-03 20:32:25 -0400

    Adding the registration service component
    Setup the mapping of register object with the form object.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterSucessComponent } from './auth/register-sucess/register-sucess.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,13 @@ import { RegisterSucessComponent } from './auth/register-sucess/register-sucess.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //Declaring all the route for the app
+    RouterModule.forRoot([
+      { path: 'register', component:RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {RegisterPayload} from "./register-payload";
+import {AuthService} from "../../auth.service";
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  registerPayload: RegisterPayload;
+
+  constructor(private formBuilder:FormBuilder, private authService:AuthService) {
+    this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    });
+    this.registerPayload = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.registerPayload.username = this.registerForm.get('username').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.registerPayload).subscribe(data=> {
+        console.log("register success");
+    }, error=>{
+        console.log("register failed");
+    });
   }
 
 }
